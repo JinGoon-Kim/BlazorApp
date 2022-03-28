69672cb2e33344807e85e3e9888dcd035b057b5b
JinGoon-Kim <raneata@gmail.com>
2022-03-28 22:18:12 +0900

    form control

--- BlazorStudy/Data/WeatherForecast.cs
+++ BlazorStudy/Data/WeatherForecast.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorStudy.Data
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        [Required(ErrorMessage = "Need TemperatureC!")]
+        [Range(typeof(int), "-100", "100")]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        [Required(ErrorMessage = "Need Summary!")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "2~10")]
         public string? Summary { get; set; }
     }
 }
