0f94fdd7ac6f19a7d7e94907cd80c5740ac4d3c3
진군 김 <vrmarine@themodoo.com>
2022-03-29 14:17:18 +0900

    State and Event

---
+++ BlazorAppState/Data/CounterState.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorAppState.Data
+{
+    public class CounterState
+    {
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count
+        {
+            get { return _count; }
+            set 
+            { 
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
